978b5e7c49a24a238198fe9102d39f5daa7592d0
Jaezer <jaezer.17@gmail.com>
2021-07-07 22:14:39 -0300

    Stage 5 Completed - RocketQ Finished

--- src/route.js
+++ src/route.js
@@ -7,10 +7,12 @@ const route = express.Router()
 route.get('/', (req,res) => res.render("index",{page: 'enter_room'}))
 route.get('/create_pass',(req,res) => res.render("index", {page: 'create_pass'}))
 
-route.get('/room/:room',(req,res) => res.render("room"))
-
-route.post('/question/:room/:question/:action', QuestionController.index)
 route.post('/create_room', RoomController.create)
+route.get('/room/:room',RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 
 module.exports = route
